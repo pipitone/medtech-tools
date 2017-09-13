2ef1e75eb8133b65410d77b8391d5de4b2de0088
Jon Pipitone <jon@pipitone.ca>
2017-09-12 20:55:58 -0400

    Bash error checking

--- run.sh
+++ run.sh
@@ -1,15 +1,25 @@
 #!/bin/bash 
 # Drive command line utilities
+set -euf -o pipefail
+
 USER="$(cat .mtuser)"
 PASS="$(cat .mtpass)"
 ICAL="$(cat .mtical)"
 
-case $1 in
+if [ "$#" == "0" ]; then
+    echo "Usage: 
+            $0 download             Download event resources
+            $0 summary              Create summary pages for given date
+    "
+    exit 1
+fi
+
+case "$1" in
     download)
         shift;
         ./download.py --user $USER --pass "$PASS" --ical "$ICAL" "$@"
         ;;
-    *)
+    summary)
         shift;
         ./summary.py --user $USER --pass "$PASS" --ical "$ICAL" "$@"
         ;;
